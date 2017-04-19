8942aefd6f50d1386e9cebb3bedb25f8cd430ebc
Samuel Weldemariam <sweldemariam@gmail.com>
2017-04-18 21:42:56 -0700

    feat(promize): add basic files for ci and test
    Issue #2

---
+++ test/unit/promize_tests.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const test = require('ava');
+
+const promize = require('../../lib/promize');
+
+test('promize.run -- runs promises that are passed', t => {
+    t.is(promize.run(), 'Hello World!');
+});
+
+
