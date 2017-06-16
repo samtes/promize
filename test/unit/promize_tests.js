e4c09a43934837abc1fd4d8529a99cf59a33c5f2
Samuel Weldemariam <seldemariam@gmail.com>
2017-06-15 23:10:16 -0700

    chore: add lint script

--- test/unit/promize_tests.js
+++ test/unit/promize_tests.js
@@ -5,7 +5,5 @@ const test = require('ava');
 const promize = require('../../lib/promize');
 
 test('promize.run -- runs promises that are passed', t => {
-    t.is(promize.run(), 'Hello World!');
+  t.is(promize.run(), 'Hello World!');
 });
-
-
